6559e225e4cfe1272d8d4446042de7c424e7f2c5
prastutsapkota <prastut.sapkota@gmail.com>
2025-04-04 11:12:00 +1100

    Added New Data

---
+++ Addition.py
@@ -0,0 +1,12 @@
+
+# Simple program for addition
+
+# Input two numbers
+num1 = float(12)
+num2 = float(25)
+
+# Perform addition
+result = num1 + num2
+
+# Display the result
+print(f"The sum of {num1} and {num2} is: {result}")
